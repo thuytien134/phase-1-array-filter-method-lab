6c79757ebafa3211e2e73250d357ecfa58416cba
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-03-25 23:06:19 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(driver,string){
+    return driver.filter(i => i.toUpperCase() === string.toUpperCase())
+}
+function fuzzyMatch(driver,string){
+    return driver.filter(i => i.startsWith(string))
+}
+function matchName(driver,string){
+    return driver.filter(driver => driver.name === string)
+}
